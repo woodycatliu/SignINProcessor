93d29b772515c4a54e110b6f95edde3f542c6b47
woodycatliu <bestlifeforzhi@gmail.com>
2023-04-04 12:12:13 +0800

    refactor: ViewModel

--- SignInProcessor/SignIn/ViewModel/SignInViewModel.swift
+++ SignInProcessor/SignIn/ViewModel/SignInViewModel.swift
@@ -8,20 +8,70 @@ import Combine
 
 class SignInViewModel {
     
-    var publisher: AnyPublisher<SignInProcessor.States, Never> {
-        return processor.publisher
+    typealias Action = SignInProcessor.Action
+    
+    typealias States = SignInProcessor.States
+    
+    struct Input {
+        let viewWillAppear: AnyPublisher<Void, Never>
+        let send: AnyPublisher<Action, Never>
     }
     
-    func send(_ action: SignInProcessor.Action) {
-        processor.send(action)
+    struct Output {
+        let states: AnyPublisher<States, Never>
     }
     
+    func transform(input: Input) -> Output {
+        bind(input)
+        return Output(states: publisher)
+    }
+  
     init(processor: ProcessorType = SignInProcessor.testProcessor) {
         self.processor = processor
     }
     
+    private var publisher: AnyPublisher<SignInProcessor.States, Never> {
+        return processor.publisher
+    }
+    
+    private func send(_ action: SignInProcessor.Action) {
+        processor.send(action)
+    }
     
     private let processor: ProcessorType
 
     typealias ProcessorType = SignInProcessor.ProcessorType
+    
+    private var bag = Set<AnyCancellable>()
+    
+}
+
+// MARK: Helper
+extension SignInViewModel {
+    
+    fileprivate func bind(_ input: Input) {
+        
+        bag = Set<AnyCancellable>()
+        
+        input.viewWillAppear
+            .bind(to: viewWillAppearBinder)
+            .store(in: &bag)
+        
+        input.send
+            .bind(to: sendAction)
+            .store(in: &bag)
+    }
+    
+    fileprivate var viewWillAppearBinder: CombineBinder<Void> {
+        return CombineBinder(self) { vm, _ in
+            vm.send(.logout)
+        }
+    }
+    
+    fileprivate var sendAction: CombineBinder<Action> {
+        return CombineBinder(self) { vm, action in
+            vm.send(action)
+        }
+    }
+    
 }
